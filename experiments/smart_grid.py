57759328aa218470ada470efa595a25bd516c884
guilhermevarela <guilhermevarela@hotmail.com>
2019-07-11 13:23:47 +0100

    benchmarks: added

--- experiments/smart_grid.py
+++ experiments/smart_grid.py
@@ -15,7 +15,7 @@ from ilu.envs.traffic_lights import TrafficLightQLGridEnv, ADDITIONAL_QL_ENV_PAR
 
 EMISSION_PATH = '/Users/gsavarela/sumo_data/'
 HORIZON = 1500
-NUM_ITERATIONS = 5
+NUM_ITERATIONS = 1
 
 
 def gen_edges(col_num, row_num):
@@ -164,7 +164,7 @@ def grid_example(render=None, use_inflows=False):
         "cars_bot": num_cars_bot
     }
 
-    sim_params = SumoParams(sim_step=0.1, render=False, print_warnings=False)
+    sim_params = SumoParams(sim_step=0.1, render=True, print_warnings=False)
 
     if render is not None:
         sim_params.render = render
