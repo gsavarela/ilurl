750362d01de94194a553d8ecae692e6b601098eb
Guilherme Varela <guilhermevarela@hotmail.com>
2019-09-30 16:37:58 +0100

    Update setup.sh with pyenv dependencies #4

--- setup.sh
+++ setup.sh
@@ -3,23 +3,38 @@
 # TODO: pass a taget directory
 # TODO: validate on a ubuntu environment
 
-# Install pyenv 
-# curl https://pyenv.run | bash
+# Install pyenv dependencies 
+sudo apt-get update && apt-get install -y \
+	make build-essential libssl-dev zlib1g-dev libbz2-dev \
+	libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \ 
+	libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev \
+	python-openssl git && curl https://pyenv.run | bash
 
-# Must install pyenv dependencies
-# sudo apt-get xxx
+# Set environment variables
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> $HOME/.bashrc &&\
+echo 'eval "$(pyenv init -)"' >> $HOME/.bashrc &&\
+echo 'eval "$(pyenv virtualenv-init -)"' >> $HOME/.bashrc
+# echo 'export PYENV_ROOT="$HOME/.pyenv"'
+# echo 'export PATH="$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH"'
+	
 # create a virtual env for the project
+pyenv install 3.6.8
 pyenv virtualenv 3.6.8 ilurl
 
-# Install FLOW+SUMO
-# Getting the last flow dependency
-git clone --depth 1 https://github.com/flow-project.flow.git $HOME/flow
-
+# Install FLOW
+# Make a new directory for flow
+cd $HOME
+mkdir flow && cd flow
+# this is the last tested version of flow
+git init && git remote add origin https://github.com/flow-project.flow.git &&\
+	 && git fetch origin 5b30957047b808128c4a6091099bebc73ff78757 --depth 1 
+# Install flow package
+pip install -e .
 # Get distro version and select proper script
 # assumption this distro is ubuntu1804
-# TODO: Check on an ubuntu1804 system
-# chmod +x $HOME/flow/scripts/setup_sumo_ubuntu1804.sh
-# $HOME/flow/scripts/setup_sumo_ubuntu1804.sh
+# TODO: Check on an ubuntu18.04 system
+chmod +x $HOME/flow/scripts/setup_sumo_ubuntu1804.sh
+$HOME/flow/scripts/setup_sumo_ubuntu1804.sh
 
 #Proof to the user everything is up-to-date
 echo "$(which sumo)"
