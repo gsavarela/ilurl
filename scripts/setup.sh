f079ef8666ce70cd216ac4118a4b675f1ba2186c
Guilherme Varela <guilhermevarela@hotmail.com>
2019-10-08 17:14:11 +0100

    Add .sh scripts for ubuntu 18.04 deployment (#4)
    NOTE:
    =====
    
    Launch on rnl.sh is not yet supported since some dependencies for
    running sumo cannot be installed without root priviledge. Two
    possible workarounds: (a) contact admin (b) deploy with docker.
    
    LOG:
    ===
    These are selected parts from running rnl.sh
    
    1)  Can't really use script as there are no sudo permission
    > Installing system dependencies for SUMO
    > scripts/setup_sumo_ubuntu1804.sh: line 4: sudo: command not found
    > scripts/setup_sumo_ubuntu1804.sh: line 5: sudo: command not found
    > scripts/setup_sumo_ubuntu1804.sh: line 6: sudo: command not found
    > scripts/setup_sumo_ubuntu1804.sh: line 7: sudo: command not found
    > scripts/setup_sumo_ubuntu1804.sh: line 8: sudo: command not found
    > scripts/setup_sumo_ubuntu1804.sh: line 9: sudo: command not found
    > scripts/setup_sumo_ubuntu1804.sh: line 10: sudo: command not found
    
    2) Paths error
    Installing sumo binaries
    ./rnl.sh: line 24: pushd: ilu/sumo_binaries/bin: No such file or directory
    
    3) Dependencies error
    sumo: error while loading shared libraries: libxerces-c-3.2.so: cannot open shared object file: No such file or directory

---
+++ scripts/setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# This bash script installs ilu project within the home folder.
+# TODO: pass a taget directory
+# TODO: include other environments
+
+
+# virtualenv for python and libsm6 for sumo
+sudo apt-get install -y virtualenv libsm6
+mkdir $HOME/venvs/
+virtualenv -p python3 $HOME/venvs/ilurl
+source $HOME/venvs/ilurl/bin/activate
+
+
+
+# Install FLOW
+# this is the lastest tested version of flow
+git clone https://github.com/flow-project/flow.git $HOME/flow &&\ 
+cd $HOME/flow && git checkout 5b30957047b && pip install -e .
+
+# Install SUMO
+# Get distro version and select proper script
+# assumption this distro is ubuntu1804
+chmod +x $HOME/flow/scripts/setup_sumo_ubuntu1804.sh &&\ 
+$HOME/flow/scripts/setup_sumo_ubuntu1804.sh && cd $HOME
+
+# Proof to the user everything is up-to-date
+source $HOME/.bashrc && echo "$(which sumo)"
+echo "$(sumo --version)"
+
+# Install ilu as a package 
+cd $HOME/ilu && pip install -r requirements.txt && pip install -e .
+# Run a sumo test
+python -m unittest discover tests/
+deactivate
