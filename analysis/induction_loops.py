fea7221879f23c5a9223debb28726c7b0b004c06
Guilherme Varela <guilhermevarela@hotmail.com>
2019-10-25 09:44:51 +0100

    Add groupby "Count" w.r.t "Time" and "ID" (#8)

--- analysis/induction_loops.py
+++ analysis/induction_loops.py
@@ -10,11 +10,14 @@ import numpy as np
 from ilurl.loaders.induction_loops import get_induction_loops
 from ilurl.utils.plots import plot_times
 
+
 def remove_time(x):
     return x.split(' ')[0]
 
+
 def remove_date(x):
     return x.split(' ')[1]
+
 
 if __name__ == '__main__':
     df = get_induction_loops()
@@ -22,7 +25,7 @@ if __name__ == '__main__':
     sensors = df.index.get_level_values('ID_Espira')
 
     print("x--------------------Header--------------------x")
-    print("Start date:" , min(dates))
+    print("Start date:", min(dates))
     print("Finish date:", max(dates))
     print("# sensors:", len(sensors))
     print(df.describe())
@@ -36,14 +39,14 @@ if __name__ == '__main__':
     df['Time'] = df.index.get_level_values('Data')
     df['Time'] = df['Time'].apply(remove_date)
 
-    table = pd.pivot_table(df, values='Count', index='Time', aggfunc=(np.mean, np.std)).reset_index()
-
+    table = pd.pivot_table(df, values='Count', index='Time',
+                           aggfunc=(np.mean, np.std)).reset_index()
 
     print(table.describe())
 
-    # place that up
     time_tick = table['Time'].values
     means = table['mean'].values
     stds = table['std'].values
 
-    plot_times(time_tick, [means, stds], ["Means", "Stds"], ["Time", "# vehicles"], "Induction Loop Readings")
+    plot_times(time_tick, [means, stds], ["Means", "Stds"],
+               ["Time", "# vehicles"], "Induction Loop Readings")
--- ilurl/loaders/induction_loops.py
+++ ilurl/loaders/induction_loops.py
@@ -2,7 +2,11 @@
 __author__ = 'Guilherme Varela'
 __date__ = '2019-10-08'
 
+import numpy as np
 import pandas as pd
+
+from datetime import datetime
+from datetime import timedelta
 
 
 def get_holidays():
@@ -17,8 +21,9 @@ def get_holidays():
                 Description: string indicating why it's a holiday
 
     """
-    df = pd.read_csv("pthol2018.txt", sep=",", 
+    df = pd.read_csv("pthol2018.txt", sep=",",
                      parse_dates=True, index_col=0, header=0, encoding="utf-8")
+
 
 def get_induction_loops(induction_loops=None, workdays=False):
     """Returns induction loops data from db
@@ -33,7 +38,7 @@ def get_induction_loops(induction_loops=None, workdays=False):
 
     RETURNS:
     --------
-    * df dataframe object
+    * df pd.DataFrame
         index: MultiIndex ("Data", "ID_Espira")
             "Data": is a datatime representation
             "ID_Espira": in format <zone_id>:<espira_number>
@@ -65,8 +70,11 @@ def get_induction_loops(induction_loops=None, workdays=False):
     """
     df = pd.read_csv('data/sensors/induction_loops.csv', sep=',', header=0)
     del df['Contadores']
-    df['ID_Espira'] = df['Zona'].apply(str) + ':' + df['ID_Espira'].replace(regex='[a-zA-Z]', value='')
+
+    df['ID_Espira'] = df['Zona'].apply(str) + ':' + \
+        df['ID_Espira'].replace(regex='[a-zA-Z]', value='')
     del df['Zona']
+
     df = df.melt(id_vars=('Data', 'ID_Espira'),
                  var_name='Time', value_name='Count')
 
@@ -75,6 +83,7 @@ def get_induction_loops(induction_loops=None, workdays=False):
     def time_fix(x):
         h, m = x.split('h')
         return f'{int(h):02d}:{int(m):02d}:00'
+
     df['Time'] = df['Time'].apply(time_fix)
     df['Data'] = df['Data'] + ' ' + df['Time']
     del df['Time']
@@ -96,6 +105,72 @@ def get_induction_loops(induction_loops=None, workdays=False):
         df = df.loc[search_index, :]
     return df
 
+
+def groupby_induction_loops(df, anchor_date=None, width=5):
+    """Groups by sensor data count for having time and id
+ 
+
+    PARAMETERS:
+    -----------
+    * df: pd.DataFrame
+        Return from get_induction_loops
+
+        index: MultiIndex ("Data", "ID_Espira")
+            "Data": is a datatime representation
+            "ID_Espira": in format <zone_id>:<espira_number>
+
+        columns:
+            "Count":  Reading
+
+    * anchor_date: datetime
+        This is the datetime (inclusive) which is the newest observation
+    * width: integer
+
+    RETURNS:
+    --------
+    * groupby_df pd.DataFrame
+        index: DateTime
+
+    """
+    # handle input argument
+    if anchor_date is None:
+        anchor_date = max(df.index.get_level_values("Data"))
+
+    if isinstance(anchor_date, str):
+        anchor_date = datetime.strptime(
+            anchor_date, "%Y-%m-%d %H:%M:%S")
+    anchor_date = anchor_date.replace(hour=0, minute=0)
+
+    # finds the upper & lower bound for window selection
+    # upper bound: midnight for the day after
+    newest = (anchor_date + timedelta(days=1)). \
+        replace(hour=0, minute=0, second=0,
+                microsecond=0)
+
+    # lower bound: midnight for the day
+    oldest = (anchor_date - timedelta(days=width - 1)). \
+        replace(hour=0, minute=0, second=0,
+                microsecond=0)
+
+    # selects only the a window with width
+    index = pd.to_datetime(df.index.get_level_values("Data"))
+    search_index = (index >= oldest) & (index < newest)
+    df = df.iloc[search_index, :]
+    df['Time'] = \
+        pd.to_datetime(df.index.get_level_values("Data")).time
+    df.reset_index(inplace=True)
+
+    # aggregates "Count" per "Time" while preserving "ID_Espira"
+    # converts from time to anchor_date
+    df = pd.pivot_table(df, columns='ID_Espira',
+                        values='Count', index='Time', aggfunc=np.mean)
+    df.index = [anchor_date + pd.to_timedelta(str(i)) for i in df.index]
+    # Rename index to the default names
+    df.index.name = 'Data'
+    df = df.stack().to_frame()
+    df.rename({0: 'Count'}, axis=1, inplace=True)
+    return df
+
 if __name__ == '__main__':
     # builds a tick graph
     df = get_induction_loops()
@@ -103,18 +178,20 @@ if __name__ == '__main__':
     assert df.equals(get_induction_loops(('3:9',)))
     assert len(df) > len((get_induction_loops(('3:9',), workdays=True)))
 
-    df.reset_index(inplace=True)
-    del df['ID_Espira']
-    df['Data'].replace(regex=' dd:dd:dd', value='', inplace=True)
-    from matplotlib import pyplot as plt
-    fig, ax = plt.subplots()
-    plt.title('Induction Loop ("Espira") 3:9')
-    #ax.plot(df['Data'], df['Count'])
-    # ax.xaxis_date()
+    # df.reset_index(inplace=True)
+    # del df['ID_Espira']
+    # df['Data'].replace(regex=' dd:dd:dd', value='', inplace=True)
+    # from matplotlib import pyplot as plt
+    # fig, ax = plt.subplots()
+    # plt.title('Induction Loop ("Espira") 3:9')
+    # ax.plot_date(df['Data'], df['Count'], marker='', linestyle='-')
 
-    # plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
+    # fig.autofmt_xdate()
 
-    ax.plot_date(df['Data'], df['Count'], marker='', linestyle='-')
+    # plt.show()
 
-    fig.autofmt_xdate()
-    plt.show()
+
+    df = groupby_induction_loops(df)
+    from IPython.core.debugger import Pdb
+    debugger = Pdb()
+    debugger.set_trace()
