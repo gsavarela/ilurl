f15eff7ee24e85f9a3405e239d3ba3fc21c045fa
Guilherme Varela <guilhermevarela@hotmail.com>
2019-10-29 16:48:20 +0000

    Add wait time to trip evaluation (#8)

--- ilurl/loaders/emission.py
+++ ilurl/loaders/emission.py
@@ -34,8 +34,6 @@ def get_emissions(scenario_id, emission_dir=None):
     df = pd.read_csv(path, sep=',', index_col=0, header=0, encoding='utf-8')
     return df
 
-
-
 def get_vehicles(emissions_df):
     """Returns vehicle data
 
@@ -43,14 +41,13 @@ def get_vehicles(emissions_df):
     -----
     ipdb> vehs_df = get_vehicles(emissions_df)
     ipdb> vehs_df.head()
-    ipdb> vehs_df.head()
-               finish  start  total
+               finish  start  wait  total
     id
-    flow_00.0    11.3    1.0   10.3
-    flow_00.1    18.4    7.1   11.3
-    flow_00.2    24.0   13.3   10.7
-    flow_00.3    29.7   19.4   10.3
-    flow_00.4    36.1   25.6   10.5
+    flow_00.0    11.3    1.0   0.0   10.3
+    flow_00.1    18.4    7.1   0.0   11.3
+    flow_00.2    24.0   13.3   0.0   10.7
+    flow_00.3    29.7   19.4   0.0   10.3
+    flow_00.4    36.1   25.6   0.0   10.5
     """
     # Builds a dataframe with vehicle starts
     start_df = pd.pivot_table(
@@ -73,9 +70,21 @@ def get_vehicles(emissions_df):
     set_index('id'). \
     rename(columns={'value': 'finish'}, inplace=False)
 
+    # Builds a dataframe with waiting times
+    wait_df = pd.pivot_table(
+        emissions_df.reset_index(),
+        columns='id', values='waiting',
+        aggfunc=max
+    ).\
+    melt(). \
+    set_index('id'). \
+    rename(columns={'value': 'wait'}, inplace=False)
+
     vehs_df = finish_df.join(
         start_df, on='id', how='inner',
-    ).sort_values('start', inplace=False)
+    ). \
+    sort_values('start', inplace=False). \
+    join(wait_df, on='id', how='left')
 
     vehs_df['total'] = vehs_df['finish'] - vehs_df['start']
     return vehs_df
@@ -91,4 +100,3 @@ if __name__ == '__main__':
         "intersection_20191029-1153521572350032.090861-emission.csv"
     df = get_emissions(intersection_id)
     vehs_df = get_vehicles(df)
-
