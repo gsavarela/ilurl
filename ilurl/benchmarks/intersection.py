ea6d48da2986efc639ea1483480341b3634850d0
Guilherme Varela <guilhermevarela@hotmail.com>
2020-01-10 11:48:36 +0000

    Parse baseline routes from intersection.rou (#13)

--- ilurl/benchmarks/intersection.py
+++ ilurl/benchmarks/intersection.py
@@ -4,45 +4,24 @@ __date__ = '2020-01-08'
 
 import os
 
-from flow.core.params import SumoParams, VehicleParams
-from flow.core.params import NetParams, InitialConfig, EnvParams
+from flow.core.params import SumoParams, VehicleParams, EnvParams
 from flow.envs import TestEnv
-from flow.scenarios import Scenario
 from flow.core.experiment import Experiment
 
+from ilurl.scenarios.intersection import IntersectionScenario
 # TODO: Generalize for any parameter
 DIR = \
     '/Users/gsavarela/Work/py/ilu/ilurl/data/networks/'
 
-
-class TemplateScenario(Scenario):
-
-    def specify_routes(self, net_params):
-        return {
-             "309265401":  ["309265401", "238059328"],
-        }
-
 if __name__ == '__main__':
     sim_params = SumoParams(render=True, sim_step=1, emission_path=DIR)
-    net_params = NetParams(
-        template={
-            'net': os.path.join(DIR, 'intersection/intersection.net.xml'),
-        },
-    )
 
     veh_params = VehicleParams()
-    veh_params.add('human', num_vehicles=1)
+    veh_params.add('human', num_vehicles=3)
     env_params = EnvParams()
 
-    initial_config = InitialConfig(
-        edges_distribution=["309265401"]
-    )
-
-
-    scenario = TemplateScenario(
-        name='Template',
-        initial_config=initial_config,
-        net_params=net_params,
+    scenario = IntersectionScenario(
+        name='intersection',
         vehicles=veh_params
     )
 
