718c37b2438255576a969a7a66ef74aabc5e189e
Varela <guilhermevarela@protonmail.com>
2020-03-04 14:14:33 +0000

    Update analytics/evaluation              (#6)

--- analysis/evaluation.py
+++ analysis/evaluation.py
@@ -3,7 +3,7 @@
 
 __author__ = 'Guilherme Varela'
 __date__ = '2020-01-22'
-
+import pdb
 import os
 import json
 from glob import glob
@@ -27,23 +27,22 @@ if __name__ == '__main__':
         for file_path in file_paths:
             with open(file_path, 'r') as f:
                 db = json.load(f)
-                rew = np.array(db['rewards']).sum(axis=1)
-                rewards.append(rew)
+                reward = np.concatenate(db['rewards'], axis=0)
+                rewards.append(reward)
 
         # convert rewards to rewards
-        rewards = np.concatenate(rewards)
-        y = np.mean(rewards, axis=0)
-        err = np.std(rewards, axis=0)
+        rewards = np.vstack(rewards).T
+        t, n = rewards.shape
+        y = np.mean(rewards, axis=1)
+        err = np.std(rewards, axis=1)
         y_error = [err, err]
         num_iterations = len(y)
-        x = [int(t / 90) * i for i in range(num_iterations)]
-
 
         # Must savefig.
         label = f'{config_dir[:2]}x{config_dir[2:]}'
         plt.xlabel('Cycles (90 sec.)')
         plt.ylabel('Reward (Km/h)')
-        plt.errorbar(x, y, yerr=y_error, fmt='-o')
+        plt.errorbar(np.arange(1, t + 1), y, yerr=y_error, fmt='-o')
         plt.title(f'Evaluation Phases: {label}')
         plt.savefig(f'{path}{label}.png')
         plt.show()
